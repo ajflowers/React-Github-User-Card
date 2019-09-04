2a3165a869b0ceb26dc7abdd817e7de462e91740
Angela Flowers <me@angela.flowers>
2019-09-03 20:44:20 -0400

    API call functional, data passing to state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CardsList from './components/CardsList'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <img src="./assets/lambdalogo.png" alt="Lambda Logo"/>
+        <span role="img" aria-label="heart">❤️'s</span>
+        <img src="./assets/githublogo.png" alt="GitHub Logo" />
+      </div>
+      <CardsList />
     </div>
   );
 }
---
+++ github-user-card/src/components/CardsList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import axios from 'axios'
+import UserCard from './UserCard'
+
+class CardsList extends React.Component {
+    state = {
+        userName: 'ajflowers',
+        userData: [],        
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.github.com/users/${this.state.userName}`)
+        .then(res => this.setState({ userData: [res.data]}))
+        .then(() => console.log(this.state.userData))
+        .catch(err => console.log(err))
+    }
+
+    render() {
+        return(
+            <div className="card-list">
+                {this.state.userData.map(user => (
+                    <UserCard 
+                        user={user}
+                        key={user.id}
+                    />
+                ))}                
+            </div>
+        )
+    }
+
+
+}
+
+export default CardsList
